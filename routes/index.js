ac9eb975878975648d1412ad1dd3fdff48a3e292
Jacob Shay <jake_ps@comcast.net>
2015-01-15 15:15:32 -0500

    got routes working

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+var Pipedrive = require('pipedrive');
+var pipedrive = new Pipedrive.Client('22f8cf1796a11f556409fdf2393d2ff0b83bdf7e');
 
 /* GET home page. */
 router.get('/', function(req, res) {
@@ -19,7 +21,14 @@ router.post('/callreportform', function(req, res)
 
 router.post('/getcustomernamefield', function(req, res)
 {
-	console.log(req.body.data);
+	var customername = req.body.data;
+	console.log(customername);
+
+	
+	
 })
+
+
+
 
 module.exports = router;
